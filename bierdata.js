bb452a6106db06136f8b14ed6ea0aeb9b6ce055e
BCornet <brent.cornet@student.ehb.be>
2024-06-02 15:28:30 +0200

    Add destructuring
    Destructuring in updateBierInfo function

--- bierdata.js
+++ bierdata.js
@@ -1,7 +1,4 @@
-"use strict";
-
-document.addEventListener("DOMContentLoaded", () => {
-  const bierData = [
+const bierData = [
     {
       naam: "Cornet Oaked",
       alcohol: "8,5%",
@@ -50,90 +47,10 @@ document.addEventListener("DOMContentLoaded", () => {
       naam: "A Plein Verre",
       alcohol: "5,5%",
       brouwerij: "DOK Brewing Company",
-      stijl: "Sour",
+      stijl: "Sour IPA",
       beschrijving:
         "Uit de Gentse wateren ontspringt vandaag een biertje met Brusselse nasmaak. BierKultuur stelt trots haar smaakmakend kindje voor. Onze zure, hoppige bébé. Onze à plein verre. Student, liefhebber of eender wie: tiens ton verre et le bonheur en main!",
       feit: "Tijdens mijn voorzittersjaar bij de studentenvereniging BierKultuur heb ik persoonlijk dit bier gebrouwen in samenwerking met DOK Brewing Company in Gent.",
       afbeelding: "Afbeeldingen/APleinVerre.png",
     },
   ];
-
-  let selectedStijl = ""; // Variabele om de geselecteerde stijl bij te houden
-
-  function selectElementById(id) {
-    return document.getElementById(id);
-  }
-
-  function updateBierInfo(bier) {
-    // Destructuring toegepast
-    const {
-      naam,
-      alcohol,
-      brouwerij,
-      stijl,
-      beschrijving,
-      feit,
-      afbeelding
-    } = bier;
-
-    selectElementById("bier-naam").innerHTML = `<strong>${naam}</strong>`;
-    selectElementById("bier-alcohol").innerHTML = `<strong>Alcoholpercentage:</strong> ${alcohol}`;
-    selectElementById("bier-brouwerij").innerHTML = `<strong>Brouwerij:</strong> ${brouwerij}`;
-    selectElementById("bier-stijl").innerHTML = `<strong>Stijl:</strong> ${stijl}`;
-    selectElementById("bier-beschrijving").textContent = beschrijving;
-    selectElementById("bier-feit").textContent = `Leuk weetje: ${feit}`;
-    selectElementById("bier-afbeelding").src = afbeelding;
-    selectElementById("bier-afbeelding").alt = naam;
-  }
-
-  function getRandomBier() {
-    let filteredBier;
-    if (selectedStijl === "") {
-      filteredBier = bierData; // Geen stijl geselecteerd, toon alle bieren
-    } else {
-      filteredBier = bierData.filter((bier) => bier.stijl === selectedStijl);
-    }
-    const randomIndex = Math.floor(Math.random() * filteredBier.length);
-    return filteredBier[randomIndex];
-  }
-
-  function filterBierByStijl(bierStijl) {
-    selectedStijl = bierStijl; // Bijhouden van de geselecteerde stijl
-    if (bierStijl === "Alle") {
-      return bierData;
-    } else {
-      return bierData.filter((bier) => bier.stijl === bierStijl);
-    }
-  }
-
-  const popup = document.querySelector(".popup");
-  const popupError = document.querySelector(".popup-error");
-  const popupBierStijlSelect = selectElementById("popup-bierstijl-select");
-  const popupToonBierKnop = selectElementById("popup-toon-bier-knop");
-
-  // Display the popup initially
-  popup.classList.add("show");
-
-  popupToonBierKnop.addEventListener("click", () => {
-    const selectedStijl = popupBierStijlSelect.value;
-    if (selectedStijl === "") {
-      popupError.textContent = "Selecteer eerst een bierstijl.";
-      popupError.style.display = "block";
-    } else {
-      popupError.style.display = "none";
-      popup.classList.remove("show");
-      filterBierByStijl(selectedStijl);
-      const newBier = getRandomBier();
-      updateBierInfo(newBier);
-      selectElementById("geselecteerde-stijl").textContent = `Geselecteerde stijl: ${selectedStijl}`;
-    }
-  });
-
-  const initialBier = getRandomBier();
-  updateBierInfo(initialBier);
-
-  selectElementById('nieuw-bier-knop').addEventListener('click', () => {
-    const newBier = getRandomBier();
-    updateBierInfo(newBier);
-  });
-});
